e3da5a2910e1b24f61279b8698e71eae9605a86e
DmitriyBessalov <dmitriy@bessalov.ru>
2023-01-22 17:20:43 +0300

    Initial commit

---
+++ shemas.py
@@ -0,0 +1,36 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+
+class Adress1(BaseModel):
+    region: str
+    city: str
+    street_type: Optional[str]
+    street: Optional[str]
+    house_type: Optional[str]
+    house: Optional[str]
+    value: str
+    lat: float
+    lng: float
+
+
+class Salary(BaseModel):
+    from_: int
+    to: int
+    currency: str
+    gross: bool
+
+
+class Contacts(BaseModel):
+    fullName: str
+    phone: str
+    email: EmailStr
+
+
+class Data(BaseModel):
+    description: str
+    employment: str
+    address: Adress1
+    name: str
+    salary: Salary
+    contacts: Contacts
